debea17d7edd3a96c61276a9869b0d1fdffc3e46
Tinotenda Joshua Matingo <tinotendajoshua2@gmail.com>
2026-05-07 21:52:51 +0200

    chat model ordering

--- backend/jatte/models.py
+++ backend/jatte/models.py
@@ -11,6 +11,10 @@ class Chat(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
+    class Meta:
+        ordering = ['created_at']
+    
+
     def __str__(self):
         return f"{self.name}"  
    
