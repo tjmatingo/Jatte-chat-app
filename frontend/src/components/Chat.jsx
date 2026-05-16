98f78926e25c8f144eca66a6f4b691d55e9f58a8
Tinotenda Joshua Matingo <tinotendajoshua2@gmail.com>
2026-05-16 02:17:46 +0200

    styling for text box

--- frontend/src/components/Chat.jsx
+++ frontend/src/components/Chat.jsx
@@ -31,7 +31,7 @@ const Chat = () => {
     <div className="chat-hero">
         <h1 className="chatroom-name">Chat Room</h1>
         <div class="relative bg-white/10 backdrop-blur-lg border border-white/20 rounded-2xl p-2 w-1/2 h-5/7 text-white">
-             <div className="space-y-2 w-full max-w-md flex flex-col">
+             <div className="chat-log space-y-2 w-full max-w-md flex flex-col mb-4">
                 {messages.map((message, index) => (
                 <div
                     key={index}
@@ -40,6 +40,11 @@ const Chat = () => {
                     {message}
                 </div>
                 ))}
+            </div>
+
+            <div className="flex flex-row">
+                <input type="text" className=" fixed left-3 bottom-3 w-10/12 rounded-xl bg-gray-100 text-black p-2" name="message" id="chat-text" aria-multiline/>
+                <button className="fixed right-6 bottom-12 w-auto h-2 "><img  className="h-10" src="src/assets/send.svg" alt="sendbutton" /></button>
             </div>
         </div>
         
